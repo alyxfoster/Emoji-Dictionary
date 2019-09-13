621c10350c0e310b33c94fdf615595d2af4f6f9e
Alyx Foster <fostercodes@outlook.com>
2019-09-13 11:23:19 -0700

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Ty Foster on 9/13/19.
+//  Copyright © 2019 Foster Coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
